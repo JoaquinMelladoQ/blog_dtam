80eac8fd658e2012c4bbb0dee67ff824dab843ba
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-28 21:09:40 -0300

    added a MIGRATION to db

---
+++ db/migrate/20201029000747_change_description_to_text_in_posts.rb
@@ -0,0 +1,4 @@
+class ChangeDescriptionToTextInPosts < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_130552) do
+ActiveRecord::Schema.define(version: 2020_10_29_000747) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
